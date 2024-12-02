d6eb4311db5c54179d4f0d1288df3ba25060709f
Martín Stanicio <martuytimo@gmail.com>
2024-12-02 18:48:23 -0300

    create navbar component

---
+++ src/components/navbar.tsx
@@ -0,0 +1,100 @@
+"use client";
+
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import Link from "next/link";
+
+import { cn } from "@/lib/utils";
+
+import Brand from "./brand";
+import { Button } from "./ui/button";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "./ui/navigation-menu";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+
+const links = [
+  { label: "Inicio", path: "#hero" },
+  { label: "Sobre nosotros", path: "#sobre-nosotros" },
+  { label: "Sucursales", path: "#sucursales" },
+  { label: "Contacto", path: "#contacto" },
+];
+
+export default function Navbar() {
+  return (
+    <div className="flex items-center justify-between gap-8 px-8 py-4">
+      <Brand />
+
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button
+            variant="outline"
+            size="icon"
+            className="border-white/10 bg-transparent text-white backdrop-blur-sm transition-colors md:hidden"
+          >
+            <FontAwesomeIcon icon={faBars} />
+            <span className="sr-only">Menú</span>
+          </Button>
+        </SheetTrigger>
+        <SheetContent className="flex flex-col items-center justify-between gap-8">
+          <SheetHeader>
+            <Brand />
+            <div className="hidden">
+              <SheetTitle />
+              <SheetDescription />
+            </div>
+          </SheetHeader>
+
+          <NavigationMenu className="flex-initial">
+            <NavigationMenuList className="flex-col space-x-0 space-y-3">
+              {links.map(({ label, path }, i) => (
+                <NavigationMenuItem key={i}>
+                  <SheetClose asChild>
+                    <Link
+                      href={path}
+                      className={cn(navigationMenuTriggerStyle(), "text-lg")}
+                    >
+                      {label}
+                    </Link>
+                  </SheetClose>
+                </NavigationMenuItem>
+              ))}
+            </NavigationMenuList>
+          </NavigationMenu>
+        </SheetContent>
+      </Sheet>
+
+      <NavigationMenu className="hidden md:block">
+        <NavigationMenuList>
+          {links.map(({ label, path }, i) => (
+            <NavigationMenuItem key={i}>
+              <Link href={path} legacyBehavior passHref>
+                <NavigationMenuLink
+                  className={cn(
+                    navigationMenuTriggerStyle(),
+                    "border border-white/10 bg-transparent text-white backdrop-blur-sm transition-colors",
+                  )}
+                >
+                  {label}
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          ))}
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
+  );
+}

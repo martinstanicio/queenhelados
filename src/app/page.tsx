65220c123877b4fad603f067255e2707fe95609c
Martín Stanicio <martuytimo@gmail.com>
2024-12-08 20:06:31 -0300

    setup landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,33 @@
+import AboutUs from "@/components/about-us";
+import ContactForm from "@/components/contact-form";
+import Hero from "@/components/hero";
+import Sucursales from "@/components/sucursales";
+import { Separator } from "@/components/ui/separator";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <main>
+      <Hero />
+      <section
+        id="sobre-nosotros"
+        className="mx-auto max-w-4xl space-y-8 px-4 py-12"
+      >
+        <h2 className="text-center text-4xl font-bold">Sobre Nosotros</h2>
+        <AboutUs />
+      </section>
+      <Separator />
+      <section
+        id="sucursales"
+        className="mx-auto max-w-4xl space-y-8 px-4 py-12"
+      >
+        <h2 className="text-center text-4xl font-bold">Sucursales</h2>
+        <Sucursales />
+      </section>
+      <Separator />
+      <section id="contacto" className="mx-auto max-w-4xl space-y-8 px-4 py-12">
+        <h2 className="text-center text-4xl font-bold">Contacto</h2>
+        <ContactForm />
+      </section>
+    </main>
+  );
 }

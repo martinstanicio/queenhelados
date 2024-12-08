808030483b03e1e429bb47a04b6596187f010318
Martín Stanicio <martuytimo@gmail.com>
2024-12-08 15:25:26 -0300

    replace lucide react icon in accordion with font awesome icon

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -1,7 +1,8 @@
 "use client";
 
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import * as AccordionPrimitive from "@radix-ui/react-accordion";
-import { ChevronDown } from "lucide-react";
 
 import * as React from "react";
 
@@ -35,7 +36,10 @@ const AccordionTrigger = React.forwardRef<
       {...props}
     >
       {children}
-      <ChevronDown className="h-4 w-4 shrink-0 text-muted-foreground transition-transform duration-200" />
+      <FontAwesomeIcon
+        icon={faChevronDown}
+        className="h-4 w-4 shrink-0 transition-transform duration-200"
+      />
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
 ));

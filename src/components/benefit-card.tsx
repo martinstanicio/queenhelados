116d85a1ae023309bb06c297fa2984ddf179f002
Martín Stanicio <martuytimo@gmail.com>
2024-12-11 22:31:02 -0300

    create benefit card component

---
+++ src/components/benefit-card.tsx
@@ -0,0 +1,31 @@
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { HTMLAttributes } from "react";
+
+import { Card, CardDescription, CardHeader, CardTitle } from "./ui/card";
+
+export type Props = HTMLAttributes<HTMLDivElement> & {
+  title: string;
+  description: string;
+  icon: IconDefinition;
+};
+
+export default function BenefitCard({
+  title,
+  description,
+  icon,
+  ...props
+}: Props) {
+  return (
+    <Card {...props}>
+      <CardHeader className="space-y-4">
+        <CardTitle className="flex flex-col items-center gap-4 text-center">
+          <FontAwesomeIcon icon={icon} className="text-xl text-primary" />
+          <span>{title}</span>
+        </CardTitle>
+        <CardDescription className="text-center">{description}</CardDescription>
+      </CardHeader>
+    </Card>
+  );
+}
